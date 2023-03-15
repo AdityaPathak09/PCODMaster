e458be06250aa34bbe40beccfebfc010f389dcd6
AdityaPathak09 <pathakaditya1653@gmail.com>
2023-03-15 13:46:28 +0530

    commit 15/3/23 V3 Added Continuous Monitoring, some changes in legend and graph. 1. legend shifted to top 2. axis removed

--- app/src/main/java/com/example/pcodmaster/LiveGraph.java
+++ app/src/main/java/com/example/pcodmaster/LiveGraph.java
@@ -172,17 +172,17 @@ public class LiveGraph extends AppCompatActivity {
         xl.setTextColor(Color.BLACK);
         xl.setDrawGridLines(true);
         xl.setAvoidFirstLastClipping(true);
-        xl.setEnabled(false);
+        xl.setEnabled(true); //vertical
 
         YAxis leftAxis = lineChart.getAxisLeft();
         leftAxis.setTextColor(Color.BLACK);
-        leftAxis.setEnabled(false);
+        leftAxis.setEnabled(true); //left  axis
 //        leftAxis.setAxisMaximum(TOTAL_MEMORY);
 //        leftAxis.setAxisMinimum(0f);
         leftAxis.setDrawGridLines(true);
 
         YAxis rightAxis = lineChart.getAxisRight();
-        rightAxis.setEnabled(false);
+        rightAxis.setEnabled(false); //right axis
 
         // Add a limit line
 //        LimitLine ll = new LimitLine(LIMIT_MAX_MEMORY, "Upper Limit");
@@ -282,7 +282,6 @@ public class LiveGraph extends AppCompatActivity {
 //        l.setDirection(Legend.LegendDirection.LEFT_TO_RIGHT);
         l.setForm(Legend.LegendForm.CIRCLE);
         l.setVerticalAlignment(Legend.LegendVerticalAlignment.TOP);
-        l.setHorizontalAlignment(Legend.LegendHorizontalAlignment.CENTER);
 //        l.setVerticalAlignment(Legend.LegendVerticalAlignment.CENTER);
         l.setTextColor(Color.BLACK);
     }
